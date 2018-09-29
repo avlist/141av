4fa440ce47e105ad57b2f9302b279f82b7ed1a5e
avlist <43705177+avlist@users.noreply.github.com>
2018-09-30 00:33:01 +0800

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,19 @@
+$.get('db.json', function(data) {
+  $.each(data.type, function(index, value) {
+    html = '<div class="col-xl-3"><div class="card"><div class="card-body"><h5 class="card-title"><strong>' + value.name + '</strong></h5><ol class="scroll">';
+    $.each(value.list, function(index, value) {
+      html += '<li class="card-text">';
+      if(value.favicon) {
+        html += '<img src="' + value.favicon + '" height="16" width="16">';
+      }
+      html += '<a href="' + value.url + '" data-toggle="tooltip" data-placement="bottom" title="' + value.description + '" target="_blank">' + value.name + '</a>';
+      if(value.feature === true) {
+        html += '&nbsp;<span class="badge badge-pill badge-warning">💎AD</span>';
+      }
+      html += '</li>';
+    });
+    html += '</ol></div></div></div>';
+    $('.row').append(html);
+  });
+  $('[data-toggle="tooltip"]').tooltip();
+});
