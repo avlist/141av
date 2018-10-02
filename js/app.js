ee83cad019eadf5f3439ead4a0d9654e9ea02cc5
avlist <43705177+avlist@users.noreply.github.com>
2018-10-02 13:09:13 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -17,15 +17,3 @@ $.get('/db.json', function(data) {
   });
   $('[data-toggle="tooltip"]').tooltip();
 });
-
-if('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
-      // Registration was successful
-      //console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }).catch(function(err) {
-      // registration failed :(
-      //console.log('ServiceWorker registration failed: ', err);
-    });
-  });
-}
