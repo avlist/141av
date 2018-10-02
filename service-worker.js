9d9273515158b4fc5aa8aceb355b3df0fe2a7279
avlist <43705177+avlist@users.noreply.github.com>
2018-10-02 12:50:31 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,22 @@
+var CACHE_NAME = '141av-cache-v1';
+var urlsToCache = [
+  '/',
+  '/favicon.ico',
+  '/css/style.css',
+  '/js/app.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css',
+  'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.min.js'
+];
 
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
