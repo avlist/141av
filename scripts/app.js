e3158e368095d06da41515010e566d8f937b3d80
avlist <43705177+avlist@users.noreply.github.com>
2018-10-04 16:02:38 +0800

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,18 +1,22 @@
 $.get('/db.json', function(data) {
   $.each(data.type, function(index, value) {
-    html = '<div class="col-xl-3"><div class="card"><div class="card-body"><h5 class="card-title"><strong>' + value.name + '</strong></h5><ol class="scroll">';
-    $.each(value.list, function(index, value) {
-      html += '<li class="card-text">';
-      if(value.favicon) {
-        html += '<img src="' + value.favicon + '" alt="' + value.name + '" height="16" width="16">';
-      }
-      html += '<a href="' + value.url + '" data-toggle="tooltip" data-placement="bottom" title="' + value.description + '" target="_blank" rel="noopener">' + value.name + '</a>';
-      if(value.feature === true) {
-        html += '<span class="badge badge-pill badge-warning">💎AD</span>';
-      }
-      html += '</li>';
-    });
-    html += '</ol></div></div></div>';
+    if(value.html) {
+      html = '<div class="col-xl-3">' + value.html + '</div>';
+    } else {
+      html = '<div class="col-xl-3"><div class="card"><div class="card-body"><h5 class="card-title"><strong>' + value.name + '</strong></h5><ol class="scroll">';
+      $.each(value.list, function(index, value) {
+        html += '<li class="card-text">';
+        if(value.favicon) {
+          html += '<img src="' + value.favicon + '" alt="' + value.name + '" height="16" width="16">';
+        }
+        html += '<a href="' + value.url + '" data-toggle="tooltip" data-placement="bottom" title="' + value.description + '" target="_blank" rel="noopener">' + value.name + '</a>';
+        if(value.feature === true) {
+          html += '<span class="badge badge-pill badge-warning">💎AD</span>';
+        }
+        html += '</li>';
+      });
+      html += '</ol></div></div></div>';
+    }
     $('.row').append(html);
   });
   $('[data-toggle="tooltip"]').tooltip();
