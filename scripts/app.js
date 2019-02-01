45069f6d5ee1ed6ea4f82ee56bee524db829e5f7
avlist <43705177+avlist@users.noreply.github.com>
2019-02-01 18:28:14 +0800

    Update app.js
    Delaying service worker registration until after the page has loaded

--- scripts/app.js
+++ scripts/app.js
@@ -27,10 +27,12 @@ $.get('/db.json', function(data) {
 });
 
 if('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js').then(function(registration) {
-    console.log('Service worker registration succeeded:', registration);
-  }, /*catch*/ function(error) {
-    console.log('ServiceWorker registration failed:', error);
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      console.log('Service worker registration succeeded:', registration);
+    }, /*catch*/ function(error) {
+      console.log('ServiceWorker registration failed:', error);
+    });
   });
 } else {
   console.log('Service workers are not supported.');
